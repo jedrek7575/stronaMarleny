811e2c8b622f94040e0771ac8969c7aef1a0b3a4
jedrek7575 <torgetheplayer75@gmail.com>
2025-03-27 16:12:01 +0100

    g

--- script.js
+++ script.js
@@ -109,3 +109,10 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
